6ea9e16cfcc1860f67a851be8612b36b86acbb62
Scott Saunders <scott.saunders@Saunders-S.local>
2022-04-01 19:26:53 -0400

    added flex reverse styles.

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php the_title();?>	</title>
-     
     <style>
        body{background-color:#000}.all{opacity:0;-webkit-opacity:0}.open-overlay{display:block;position:fixed;top:0;left:0;width:100%;height:100vh;z-index:99999;background-color:#121212}.open-overlay-box{display:table;height:100vh;width:100%;text-align:center}.open-overlay-logo{display:table-cell;vertical-align:middle;opacity:.2;-webkit-opacity:.2}.open-overlay-logo img{max-width:120px!important;width:90%}.btt-footer{display: none;}
        </style>
