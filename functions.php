625c6d4929b3310402d6a7b2b058678e5247feff
Scott Saunders <scott.saunders@Saunders-S.local>
2022-04-04 10:30:13 -0400

    created class to address flex reverse.

--- functions.php
+++ functions.php
@@ -57,6 +57,14 @@ function wphelp_compatibility_new_image_formats( $mime_types ) {
 }
 add_filter( 'upload_mimes', 'wphelp_compatibility_new_image_formats', 1, 1 );
 
+function add_file_types_to_uploads($file_types){
+$new_filetypes = array();
+$new_filetypes['svg'] = 'image/svg+xml';
+$file_types = array_merge($file_types, $new_filetypes );
+return $file_types;
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
+
 
 function preview_stuff()
 	// Adds preview stuff.
